9972b312d8565fbe50fe94ba1913a12c156c2a7f
Ubuntu <ubuntu@ip-172-31-4-166.us-east-2.compute.internal>
2021-01-04 19:14:42 +0000

    User index and show add

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   
   # get '/signup', to: 'users#new'
-  devise_for :users
-  # , controllers: {
-  #     confirmations:  'users/confirmations',
-  #     sessions:       'users/sessions',
-  #     passwords:      'users/passwords',
-  #     registrations:  'users/registrations'
-  # }
+  devise_for :users, controllers: {
+      confirmations:  'users/confirmations',
+      sessions:       'users/sessions',
+      passwords:      'users/passwords',
+      registrations:  'users/registrations'
+  }
+  resources :users, only: [:index,:show]
+  # get '/users', to: 'users#index'
   get '/home', to: 'static_pages#home'
   get '/about', to: 'static_pages#about'
   get '/description', to: 'static_pages#description'
