a3a5aea9d02c70fb1b158d8e017547e4cbc34f14
Ubuntu <ubuntu@ip-172-31-7-105.us-east-2.compute.internal>
2021-03-03 05:44:43 +0000

    s3_endpoint: s3-us-east-2.amazonaws.com

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -5,6 +5,7 @@ aws = {
   secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
   region: "us-east-2",
   bucket: "cat-app-refile",
+  s3_endpoint: "s3-us-east-2.amazonaws.com"
 }
 Refile.cache = Refile::S3.new(prefix: "cache", **aws)
 Refile.store = Refile::S3.new(prefix: "store", **aws)
