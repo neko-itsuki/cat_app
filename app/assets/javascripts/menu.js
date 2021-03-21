97cefbc10a7e6c2afdbd350b1d694dbd3a3408e3
Ubuntu <ubuntu@ip-172-31-32-213.ap-northeast-1.compute.internal>
2021-03-21 21:11:47 +0000

    layout modify

--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -1,22 +1,22 @@
 /* global $*/
-document.addEventListener("turbolinks:load", function () {
-  $(".menu-trigger").click(function () {
-    $(this).toggleClass("active");
-    $(this).next().toggleClass("onanimation");
-    $('ul li').hide();
-    $('ul li').each(function(i) {
-      $(this).delay(80 * i).fadeIn(500);
-    });
-    $($(this).next(".menus")).animate(
-      {height: "toggle", opacity: "toggle"},
+// document.addEventListener("turbolinks:load", function () {
+//   $(".menu-trigger").click(function () {
+//     $(this).toggleClass("active");
+//     $(this).next().toggleClass("onanimation");
+//     $('ul li').hide();
+//     $('ul li').each(function(i) {
+//       $(this).delay(80 * i).fadeIn(500);
+//     });
+//     $($(this).next(".menus")).animate(
+//       {height: "toggle", opacity: "toggle"},
   
-      "nomal"
-    );
-  });
-});
+//       "nomal"
+//     );
+//   });
+// });
 
 document.addEventListener("turbolinks:load", function() {
   var height=$("#header").height();
-  $("body").css("margin-top", height);
+  $(".main").css("margin-top", height);
 });
 
