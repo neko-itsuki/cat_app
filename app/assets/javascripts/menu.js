7a3685ff36c37ab1eed704a815e10a34ed11ad7d
Ubuntu <ubuntu@ip-172-31-32-213.ap-northeast-1.compute.internal>
2021-03-29 06:52:10 +0000

    layout add

--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -1,20 +1,4 @@
 /* global $*/
-// document.addEventListener("turbolinks:load", function () {
-//   $(".menu-trigger").click(function () {
-//     $(this).toggleClass("active");
-//     $(this).next().toggleClass("onanimation");
-//     $('ul li').hide();
-//     $('ul li').each(function(i) {
-//       $(this).delay(80 * i).fadeIn(500);
-//     });
-//     $($(this).next(".menus")).animate(
-//       {height: "toggle", opacity: "toggle"},
-  
-//       "nomal"
-//     );
-//   });
-// });
-
 document.addEventListener("turbolinks:load", function() {
   var height=$("#header").height();
   $(".main").css("margin-top", height);
