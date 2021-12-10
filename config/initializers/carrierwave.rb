20bba5d22f06c873e631bc46ae9565b26b870dd5
neko-itsuki <nekoitsuki19991202@gmail.com>
2021-12-10 16:38:38 +0000

    S3 バケット名セットアップ

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,12 +2,12 @@ unless Rails.env.development? || Rails.env.test?
   CarrierWave.configure do |config|
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: 'AKIA3LT2D4X7TFQJ45ME',
-      aws_secret_access_key: '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',
+      aws_access_key_id: 'AKIARVPZP2ML3VGUZFSH',
+      aws_secret_access_key: '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',
       region: 'ap-northeast-1'
     }
 
-    config.fog_directory  = 'cat-app-refile'
+    config.fog_directory  = 'cat-app-bucket'
     config.cache_storage = :fog
   end
 end
